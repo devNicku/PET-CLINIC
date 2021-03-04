a8e759921d9151c3ef653c4e428f6a9a2581d981
devNicku <stsnicky@gmail.com>
2021-03-03 19:03:06 -0800

    rest Controller

--- pet-clinic/src/main/java/com/example/petclinic/controllers/MainController.java
+++ pet-clinic/src/main/java/com/example/petclinic/controllers/MainController.java
@@ -9,21 +9,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.petclinic.components.EmailServiceComponent;
 import com.example.petclinic.models.User;
 import com.example.petclinic.services.UserService;
 import com.example.petclinic.validator.UserValidator;
-
-import com.example.petclinic.components.EmailServiceComponent;
 
 @Controller
 public class MainController {
 	@Autowired
 	private EmailServiceComponent sendEmail;
+	
+	@Autowired
+	private UserValidator userValidator;
+	
+	@Autowired
+	private UserService userService;
+	
+//	public MainController(UserService userService, UserValidator userValidator) {
+//		this.userService = userService;
+//		this.userValidator = userValidator;
+//	}
 	
 	@GetMapping("/")
 	public String index() {
@@ -34,22 +45,6 @@ public class MainController {
 	public String appoint() {
 		return "appointment.jsp";
 	}
-
-	UserService userService;
-	 
-	@Autowired
-	 private UserValidator userValidator;
-	 
-	 public MainController(UserService userService, UserValidator userValidator) {
-	        this.userService = userService;
-	        this.userValidator = userValidator;
-	 }
-	
-	
-	@RequestMapping("/registration")
-    public String registerForm(@Valid @ModelAttribute("user") User user) {
-        return "registrationPage.jsp";
-    }
 	
 	@RequestMapping("/register")
     public String registerAdminForm(@Valid @ModelAttribute("user") User user) {
@@ -57,48 +52,48 @@ public class MainController {
     }
     
 	@RequestMapping("/login")
-    public String login(@RequestParam(value="error", required=false) String error, @RequestParam(value="logout", required=false) String logout, Model model) {
+    public String login(@Valid @ModelAttribute("user") User user, @RequestParam(value="error", required=false) String error, @RequestParam(value="logout", required=false) String logout, Model model) {
         if(error != null) {
             model.addAttribute("errorMessage", "Invalid Credentials, Please try again.");
         }
         if(logout != null) {
             model.addAttribute("logoutMessage", "Logout Successful!");
         }
-        return "loginPage.jsp";
+        return "loginReg.jsp";
     }
 	
-	 @PostMapping("/registration")
-	  public String registration(@Valid @ModelAttribute("user") User user, BindingResult result, Model model, HttpSession session) {
-		 userValidator.validate(user, result);
-		 if (result.hasErrors()) {
-	            return "registrationPage.jsp";
-	        }
-		  userService.saveWithUserRole(user);
-	        return "redirect:/login";
-	 }
+	@PostMapping("/registration")
+	public String registration(@Valid @ModelAttribute("user") User user, BindingResult result, Model model, HttpSession session) {
+		userValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "loginReg.jsp";
+		}
+		userService.saveWithUserRole(user);
+		return "redirect:/";
+	}
 	 
-	 @PostMapping("/register")
-	  public String registrationAdmin(@Valid @ModelAttribute("user") User user, BindingResult result, Model model, HttpSession session, @RequestParam("ocupation") long number) {
-		 userValidator.validate(user, result);
-		 if (result.hasErrors()) {
-	            return "registrationPage.jsp";
-	        }
-		  userService.saveUserWithAdminRole(user, number);
-	        return "redirect:/login";
-	 }
+	@PostMapping("/register")
+	public String registrationAdmin(@Valid @ModelAttribute("user") User user, BindingResult result, Model model, HttpSession session, @RequestParam("ocupation") long number) {
+		userValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "loginReg.jsp";
+		}
+		userService.saveUserWithAdminRole(user, number);
+		return "redirect:/login";
+	}
 	 
-	 @RequestMapping(value = {"/", "/home"})
-	    public String home(Principal principal, Model model) {
-	        String username = principal.getName();
-	        model.addAttribute("currentUser", userService.findByUsername(username));
-	        return "index.jsp";
-	    }
-	 
-	 @RequestMapping("/admin")
-	    public String adminPage(Principal principal, Model model) {
-	        String username = principal.getName();
-	        model.addAttribute("currentUser", userService.findByUsername(username));
-	        return "adminPage.jsp";
-	    }	
+	@RequestMapping(value = {"/", "/home"})
+	public String home(Principal principal, Model model) {
+		String username = principal.getName();
+		model.addAttribute("currentUser", userService.findByUsername(username));
+		return "index.jsp";
+	}
+
+	@RequestMapping("/admin")
+	public String adminPage(Principal principal, Model model) {
+		String username = principal.getName();
+		model.addAttribute("currentUser", userService.findByUsername(username));
+		return "adminPage.jsp";
+	}	
 
 }
